96220f2531ad8405af9657e5195a6b233eabcacc
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-05-01 22:35:00 +0100

    create screens

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View } from "react-native";
 import FeedScreen from "./src/screens/FeedScreen";
+import CreatePostScreen from "./src/screens/CreatePostScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FeedScreen />
+      <CreatePostScreen />
 
       <StatusBar style="auto" />
     </View>
